8316683a9d12dcd0ebb25c797a0513a3c17ce528
翁Victor <43628067+Victor81528@users.noreply.github.com>
2022-04-11 18:01:45 +0800

    no message

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -11,13 +11,7 @@
           {{i.fav}}
         </div> -->
         <div class="col-6 col-md-4 col-lg-3 col-xl-2 position-relative mb-5" v-for="item of products" :key="item.id">
-          <img :src="item.img" @click="toShopinfo(item.id)">
-          <h4 class="mt-2 mb-3 ms-1">{{item.id}}</h4>
-          <div class="d-flex justify-content-between align-items-center ms-1 me-1">
-            <p class="mb-0">$ {{item.price}}</p>
-            <i v-if="!item.fav" class="fa-regular fa-heart text-secondary" @click="addFav(item.id)"></i>
-            <i v-else class="fa-solid fa-heart" @click="removeFav(item.id)"></i>
-          </div>
+          <productCard :item="item" />
         </div>
       </div>
     </div>
@@ -27,71 +21,29 @@
 <script>
 import { computed } from 'vue'
 import { useStore } from 'vuex'
-import { useRoute, useRouter } from 'vue-router'
+import { useRoute } from 'vue-router'
+import productCard from '@/components/productCard'
 
 export default {
   name: 'Shop',
+  components: {
+    productCard
+  },
   setup () {
     const store = useStore()
     const route = useRoute()
-    const router = useRouter()
     const group = computed(() => route.params.group)
     const type = computed(() => route.params.type)
     const products = computed(() => store.getters[`products/${type.value}`])
 
-    const addFav = (id) => {
-      // 將該產品id加入localStorage
-      store.commit('favorite/add', id)
-      // 重新取得localStorage的資料
-      store.commit('favorite/getData')
-    }
-
-    const removeFav = (id) => {
-      store.commit('favorite/remove', id)
-      store.commit('favorite/getData')
-    }
-
-    const toShopinfo = (id) => {
-      router.push({
-        name: 'Shop_info',
-        path: `/shopinfo/${id}`,
-        params: {
-          id: id
-        }
-      })
-    }
     return {
       group,
       type,
-      products,
-      addFav,
-      removeFav,
-      toShopinfo
+      products
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  img {
-    width: 100%;
-    height: 250px;
-    border-radius: 5px;
-    object-fit: cover;
-    -webkit-user-drag: none;
-  }
-  h4 {
-    font-family: 'Jost';
-    font-weight: 400;
-  }
-  p {
-    font-family: 'Jost';
-    font-weight: 600;
-  }
-  i {
-    cursor: pointer;
-    &:hover {
-
-    }
-  }
 </style>
