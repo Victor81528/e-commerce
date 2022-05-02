6ef0640c6709a3fbee240777c17c010830d1bd66
翁Victor <43628067+Victor81528@users.noreply.github.com>
2022-05-02 12:36:39 +0800

    no message

--- src/store/banner.js
+++ src/store/banner.js
@@ -10,37 +10,37 @@ const state = {
       title: 'Bolas <span style="color: black">x</span> Kjølig',
       img: process.env.VUE_APP_PATH + '/img/banner/02.jpeg',
       linkTitle: 'Discover the new collection',
-      link: process.env.VUE_APP_PATH + '/women/clothing'
+      link: '/women/clothing'
     },
     {
       title: '2022 SPRING',
       img: process.env.VUE_APP_PATH + '/img/banner/03.jpeg',
       linkTitle: '<span style="color: black; font-weight: 600">Discover the new collection</span>',
-      link: process.env.VUE_APP_PATH + '/women/all'
+      link: '/women/all'
     },
     {
       title: '<span style="color: black">Outdoor Series<span>',
       img: process.env.VUE_APP_PATH + '/img/banner/04.jpeg',
       linkTitle: '<span style="color: black; font-weight: 600">Discover the new collection</span>',
-      link: process.env.VUE_APP_PATH + '/all/bag'
+      link: '/all/bag'
     },
     {
       title: '<span">Accessories<span>',
       img: process.env.VUE_APP_PATH + '/img/banner/05.jpeg',
       linkTitle: '<span style="color: black; font-weight: 600">Discover the new collection</span>',
-      link: process.env.VUE_APP_PATH + '/all/shoes'
+      link: '/all/shoes'
     },
     {
       title: '<span style="color: black">Ocalou Design<span>',
       img: process.env.VUE_APP_PATH + '/img/banner/06.jpeg',
       linkTitle: '<span style="color: black; font-weight: 600">Discover the new collection</span>',
-      link: process.env.VUE_APP_PATH + '/women/clothing'
+      link: '/women/clothing'
     },
     {
       title: "<span>Man's Soul<span>",
       img: process.env.VUE_APP_PATH + '/img/banner/07.jpeg',
       linkTitle: '<span>Discover the new collection</span>',
-      link: process.env.VUE_APP_PATH + '/men/shoes'
+      link: '/men/shoes'
     }
   ]
 }
